62d5ef46d086da9c4d2b61838399ae4c96887667
jules blanc <jules.blanc@unccub.com>
2019-10-04 01:52:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,109 @@
+def find_pigeon_name(data)
+  name_found=[]
+  output=[]
+  
+  data.each do |attributes, options|
+    options.each do |types, names|
+      names.each do |name|
+        name_found.push(name)
+      end
+    end
+  end
+  
+  output=name_found.uniq
+  output
+end
+
+
+def collect_attributes_of_pidgeon(data,selected_name)
+  output=[]
+  attributes_list=[]
+  
+  data.each do |attribut, options|
+    options.each do |option, names|
+      names.each do |name|
+        if name==selected_name
+          attributes_list.push(attribut)
+        end
+      end
+    end
+  end  
+  output=attributes_list.uniq
+  output
+end
+
+def collect_colors(data, selected_name)
+  output=[]
+  color_list=[]
+  
+  data[:color].each do |shade, names|
+    names.each do |name|
+      if name==selected_name
+        color_list.push(shade.to_s)
+      end
+    end
+  end
+  
+  output=color_list
+end
+
+def collect_gender(data, selected_name)
+  output=[]
+  gender=[]
+
+  data[:gender].each do |sex, names|
+    names.each do |name|
+      if name==selected_name
+        gender.push(sex.to_s)
+      end
+    end
+  end
+  
+  output=gender
+end
+
+def collect_lives(data, selected_name)
+  output=[]
+  lives=[]
+
+  data[:lives].each do |location, names|
+    names.each do |name|
+      if name==selected_name
+        lives.push(location)
+      end
+    end
+  end
+  
+  output=lives
+end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ 
+ output_hash={}
+ names=[]
+ 
+ names=collect_unique_pidgeon_names(data)
+ 
+ names.each do |name|
+  output_hash[name]={}
+ end
+ 
+ output_hash.each do |name, attributes|
+   pidgeon_keys=collect_attributes_of_pidgeon(data,name)
+   
+   pidgeon_keys.each do |key, value|
+    output_hash[name][key]=[]
+   end
+   
+   colors=collect_colors(data, name)
+   output_hash[name][:color]=colors
+   
+   gender=collect_gender(data, name)
+   output_hash[name][:gender]=gender
+   
+   lives=collect_lives(data, name)
+   output_hash[name][:lives]=lives
+ end
+ 
+ output_hash
 end
