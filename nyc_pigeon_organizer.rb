4fe756e47ba0cf8efdcc6bd7d487eb0d4a10632d
jules blanc <jules.blanc@unccub.com>
2019-10-04 01:54:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,7 @@
 def nyc_pigeon_organizer(data)
-  def find_pigeon_names 
+  data.each do |attributes, value2| value2.each do |types, names| names.each do |name| name_list.push(name)
+      end
+    end
+    
+  
 end
